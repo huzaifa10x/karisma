c56c7728616aa31f14b9b8b80f2bdbf351ee9362
huzaifa <syeddaniyalali078@gmail.com>
2026-05-21 19:07:26 +0500

    jhglkj

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,14 +1,16 @@
 import type { PageServerLoad } from "./$types.js";
 
 export const load: PageServerLoad = async ({ fetch }: Parameters<PageServerLoad>[0]) => {
-    const [homeRes, doctorRes, serviceRes, departmentsRes] = await Promise.all([
+    const [homeRes, doctorRes, serviceRes, departmentsRes, metaRes] = await Promise.all([
         fetch("https://admin.karismamc.com/api/homepage"),
         fetch("https://admin.karismamc.com/api/doctors"),
         fetch("https://admin.karismamc.com/api/homepage?banner_type=department"),
         fetch("https://admin.karismamc.com/api/departments"),
+        fetch("https://admin.karismamc.com/public/api/metatagall"),
+
     ]);
 
-    if (!homeRes.ok || !doctorRes.ok || !serviceRes.ok || !departmentsRes.ok) {
+    if (!homeRes.ok || !doctorRes.ok || !serviceRes.ok || !departmentsRes.ok || !metaRes.ok) {
         throw new Error("Failed to fetch data");
     }
 
@@ -16,13 +18,15 @@ export const load: PageServerLoad = async ({ fetch }: Parameters<PageServerLoad>
     const doctors = await doctorRes.json();
     const service = await serviceRes.json();
     const departments = await departmentsRes.json();
+    const meta = await metaRes.json();
 
-    console.log("Server Data:", { homepage, doctors, service, departments });
+    console.log("Server Data:", { homepage, doctors, service, departments, meta });
 
     return {
         homepage,
         doctors,
         service,
-        departments
+        departments,
+        meta
     };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,18 @@
   import Services from "$lib/components/home/Services.svelte";
   import OurDoctors from "$lib/components/home/OurDoctors.svelte";
   let { data } = $props();
+  console.log(data.meta.data[0]);
 </script>
+
+<svelte:head>
+  <title>{data?.meta?.data[0]?.meta_title_ar}</title>
+  <meta name="description" content={data?.meta?.data[0]?.meta_description_ar} />
+  <!-- <meta property="og:title" content={post.metaTitle} /> -->
+  <!-- <meta property="og:description" content={post.metaDescription} /> -->
+  <!-- <meta property="og:image" content={post.image} /> -->
+
+  <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
 
 <Navbar />
 <MainHeroSlider items={data.homepage} />
